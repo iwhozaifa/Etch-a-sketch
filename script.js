0da96d9d0dabccb14a54baad672e24aaefb135cd
iwhozaifa <alihuzaifa7204@gmail.com>
2026-04-17 12:57:20 +0500

    Fixed the move hover logic
    Included the mouse hover logic inside the loop of box creation

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const container = document.querySelector("#container")
 for(let i = 0; i < 256; i++){
   const box = document.createElement("div")
   box.classList.add("boxes")
-  container.appendChild(box)
-}
-
-const cont = document.querySelectorAll(".boxes")
-
-cont.forEach(box => {
   
-box.addEventListener('mouseover', () =>{
-  box.style.backgroundColor = ""
-})
-
 box.addEventListener('mouseenter', () => {
   box.style.backgroundColor = "red"
 })
-});
+
+box.addEventListener('mouseleave', () =>{
+  box.style.backgroundColor = ""
+})
+
+  container.appendChild(box)
+}
+
+// const cont = document.querySelectorAll(".boxes")
+//
+// cont.forEach(box => {
+//
+// box.addEventListener('mouseover', () =>{
+//   box.style.backgroundColor = ""
+// })
+//
+// box.addEventListener('mouseenter', () => {
+//   box.style.backgroundColor = "red"
+// })
+// });
