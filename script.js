80a1f7b1d9090777f6e2af075925fd9b669b0b8b
iwhozaifa <alihuzaifa7204@gmail.com>
2026-04-17 13:05:24 +0500

    Used event delegation for color manipulation

--- script.js
+++ script.js
@@ -3,28 +3,19 @@ const container = document.querySelector("#container")
 
 for(let i = 0; i < 256; i++){
   const box = document.createElement("div")
-  box.classList.add("boxes")
-  
-box.addEventListener('mouseenter', () => {
-  box.style.backgroundColor = "red"
-})
-
-box.addEventListener('mouseleave', () =>{
-  box.style.backgroundColor = ""
-})
-
+  box.classList.add("boxes") 
   container.appendChild(box)
 }
 
-// const cont = document.querySelectorAll(".boxes")
-//
-// cont.forEach(box => {
-//
-// box.addEventListener('mouseover', () =>{
-//   box.style.backgroundColor = ""
-// })
-//
-// box.addEventListener('mouseenter', () => {
-//   box.style.backgroundColor = "red"
-// })
-// });
+container.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains("boxes")){
+    e.target.style.backgroundColor = "red"
+  }
+})
+
+container.addEventListener("mouseout", (e) => {
+  if (e.target.classList.contains("boxes")) {
+    e.target.style.backgroundColor = ""
+  }
+})
+
