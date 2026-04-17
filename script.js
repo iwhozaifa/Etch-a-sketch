d57106783228526594985e4c1e3eb36d8fadeacc
iwhozaifa <alihuzaifa7204@gmail.com>
2026-04-17 14:16:06 +0500

    Added styling and fonts

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const container = document.querySelector("#container")
 
-
+//Loop for creating the boxes
 for(let i = 0; i < 256; i++){
   const box = document.createElement("div")
   box.classList.add("boxes") 
+  box.dataset.count = 0
   container.appendChild(box)
 }
 
+//Function for controlling the event listeners 
 container.addEventListener('mouseover', (e) => {
-  if (e.target.classList.contains("boxes")){
-    e.target.style.backgroundColor = "red"
-  }
-})
+  const target = e.target
 
-container.addEventListener("mouseout", (e) => {
-  if (e.target.classList.contains("boxes")) {
-    e.target.style.backgroundColor = ""
+  if (target.classList.contains("boxes")){
+    let count = parseInt(target.dataset.count);
+    
+    if (count < 10) {
+      count++
+      target.dataset.count = count
+      const lightness = 100 - (count * 10)
+      const hue = count * 36
+  
+      target.style.backgroundColor = `hsl(${hue}, 100%, ${lightness}%)`
+    }
   }
-})
+});
 
+// container.addEventListener("mouseout", (e) => {
+//   if (target.classList.contains("boxes")) {
+//     target.style.backgroundColor = ""
+//   }
